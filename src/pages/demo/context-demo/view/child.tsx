1df29a7869c0ad61f6820165c653b7a7c3c39099
jiafeng <2621275142@qq.com>
2023-05-05 09:44:40 +0800

    1

--- src/pages/demo/context-demo/view/child.tsx
+++ src/pages/demo/context-demo/view/child.tsx
@@ -1,8 +1,15 @@
+/*
+ * @Author: jiafeng 2621275142@qq.com
+ * @Date: 2022-09-30 15:17:44
+ * @LastEditors: jiafeng 2621275142@qq.com
+ * @LastEditTime: 2023-02-08 15:44:49
+ * @FilePath: \vite-react\src\pages\demo\context-demo\view\child.tsx
+ */
 import { useUIContext } from '../ui-context';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useImperativeHandle, forwardRef } from 'react';
 import { Button } from 'antd';
 
-export const Child = () => {
+export const Child = forwardRef((props, cref) => {
     const UIContext = useUIContext();
     const UIProps = useMemo(() => {
         return {
@@ -16,6 +23,15 @@ export const Child = () => {
     const handleClick = () => {
         UIProps.funLog()
     }
+
+    const [visible, setVisible] = useState(false);
+    useImperativeHandle(cref, () => ({
+        // model 就是暴露给父组件的方法
+        model: () => {
+          setVisible(true);
+        }
+      }));
+  
     return (
         <>
             {
@@ -23,6 +39,7 @@ export const Child = () => {
             }
             <Button type="primary" onClick={handleClick}>测试</Button>
             <Button type="primary" onClick={UIProps.change}>change</Button>
+            {visible.toString()}
         </>
     )
-} 
+})
--- src/pages/demo/context-demo/view/index.tsx
+++ src/pages/demo/context-demo/view/index.tsx
@@ -1,14 +1,29 @@
+/*
+ * @Author: jiafeng 2621275142@qq.com
+ * @Date: 2022-09-30 15:10:43
+ * @LastEditors: jiafeng 2621275142@qq.com
+ * @LastEditTime: 2023-02-08 15:43:48
+ * @FilePath: \vite-react\src\pages\demo\context-demo\view\index.tsx
+ */
 import { UIProvider } from '../ui-context';
 import { Child } from './child'
+import { useRef } from 'react'
 const ContextDemo = () => {
     const UIEvents = {
         funLog: () => {
             console.log('测试')
         }
     }
+
+    const modelRef: any = useRef(null);
+    const handleModel = () => {
+        modelRef.current.model()
+    }
+
     return (
         <UIProvider UIEvents={UIEvents}>
-            <Child />
+            <Child ref={modelRef} />
+            <button onClick={handleModel}>确定</button>
         </UIProvider>
     )
 }
